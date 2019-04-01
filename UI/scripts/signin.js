316e46bfb74f0170131de885ae4f7c92fc2179df
ovieMudi <oviisaacs@gmail.com>
2019-04-01 12:59:36 +0100

    Add signin and signup pages

---
+++ UI/scripts/signin.js
@@ -0,0 +1,8 @@
+const toggleForm = document.querySelectorAll(".js-tgl-form");
+const container = document.getElementById("js-container");
+
+toggleForm.forEach(click => {
+  click.addEventListener("click", () => {
+    container.classList.toggle("right-box-active");
+  });
+});

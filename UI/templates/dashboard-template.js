b2640115c760e9845167e05286099feb34d64ffc
Mudi <42499892+OvieMudi@users.noreply.github.com>
2019-04-03 10:02:44 +0100

    #164979262 client dashboard page (#2)
    * Create client dashboard page
    
    * Add favicon to html pages

---
+++ UI/templates/dashboard-template.js
@@ -0,0 +1,6 @@
+const navbarToggle = document.getElementById("js-navbar-toggle");
+const mainNavlinks = document.getElementById("js-navlinks");
+
+navbarToggle.addEventListener("click", () => {
+  mainNavlinks.classList.toggle("visible");
+});

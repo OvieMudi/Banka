f2e24e7bca3972058100aac9ac7ee30c1e332e9c
ovieMudi <oviisaacs@gmail.com>
2019-04-06 11:19:51 +0100

    Add Transactions page
    Included a modal for creating new account

---
+++ UI/scripts/table.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // General table
+  const rows = document.querySelectorAll("tr[data-href]");
+
+  rows.forEach(row => {
+    row.addEventListener("click", function() {
+      window.location.href = this.dataset.href;
+    });
+  });
+});
